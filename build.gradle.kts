83e2ad4c24c943720e7f056ddf65c3cb804f4006
Hugo Cardoso <hugorafaelmc@gmail.com>
2022-01-31 18:26:30 -0300

    update open api

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springdoc:springdoc-openapi-data-rest:1.6.0")
-	implementation("org.springdoc:springdoc-openapi-ui:1.6.0")
-	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.0")
+	implementation("org.springdoc:springdoc-openapi-data-rest:1.6.3")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 }
