547c0d8e0beb353c45c1ebdd616bb532da0e0bf9
Hugo Cardoso <hugorafaelmc@gmail.com>
2022-02-02 13:38:31 -0300

    separate controllers

--- src/main/kotlin/com/hcardoso/cart/controllers/CartController.kt
+++ src/main/kotlin/com/hcardoso/cart/controllers/CartController.kt
@@ -1,6 +1,5 @@
 package com.hcardoso.cart.controllers
 
-import com.hcardoso.cart.exceptions.ProductNotFoundException
 import com.hcardoso.cart.models.Cart
 import com.hcardoso.cart.models.CartItem
 import com.hcardoso.cart.models.CartItemSimple
@@ -60,121 +59,6 @@ class CartController(
     ): ResponseEntity<CartSchema> {
         val cartResponse = cartService.getById(id)
         return ResponseEntity.ok(cartResponse)
-    }
-
-    @GetMapping("/{id}/products")
-    fun getProducts(
-        @PathVariable id: String
-    ): ResponseEntity<ArrayList<CartItem>> {
-        return try {
-            val cartResponse = cartService.getById(id)
-            ResponseEntity.ok(cartResponse.items)
-        } catch (e: Exception) {
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null)
-        }
-    }
-
-    @PostMapping("/{id}/products")
-    fun addProducts(
-        @PathVariable id: String,
-        @RequestBody items: ArrayList<CartItemSimple>
-    ): ResponseEntity<CartSchema> {
-        val cartResponse = cartService.getById(id)
-        val cart = Cart(
-            id = cartResponse.id,
-            items = cartResponse.items
-        )
-
-        for(item in items) {
-            val cartItem = cart.items.find { it.product.id == item.productId }
-
-            if (cartItem != null) {
-                cart.changeQuantity(item.productId, item.quantity + cartItem.quantity)
-            } else {
-                val product = productService.getById(item.productId)
-                cart.addItem(
-                    CartItem(
-                        product = ProductSchema(
-                            id = product.id,
-                            name = product.name,
-                            price = product.price
-                        ),
-                        quantity = item.quantity
-                    )
-                )
-            }
-        }
-
-        return ResponseEntity.ok(
-            cartService.update(
-                CartSchema(
-                    id = cart.id,
-                    items = cart.items,
-                    total = cart.getTotal()
-                )
-            )
-        )
-    }
-
-    @PutMapping("/{id}/products/{itemId}")
-    fun editProduct(
-        @PathVariable id: String,
-        @PathVariable itemId: String,
-        @RequestParam quantity: String
-    ): ResponseEntity<Nothing> {
-        val cartSchema = cartService.getById(id)
-
-        val cart = Cart(
-            id = cartSchema.id,
-            items = cartSchema.items
-        )
-
-        val item = cart.items.find { it.product.id == itemId }
-
-        if (item != null) {
-            cart.changeQuantity(itemId, quantity.toInt())
-
-            cartService.update(
-                CartSchema(
-                    id = cart.id,
-                    items = cart.items,
-                    total = cart.getTotal()
-                )
-            )
-        }
-
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(null)
-    }
-
-    @DeleteMapping("/{id}/products/{itemId}")
-    fun removeProduct(
-        @PathVariable id: String,
-        @PathVariable itemId: String
-    ): ResponseEntity<Nothing> {
-        val cartSchema = cartService.getById(id)
-
-        val cart = Cart(
-            id = cartSchema.id,
-            items = cartSchema.items
-        )
-
-        val item = cart.items.find { it.product.id == itemId }
-
-        if (item != null) {
-            cart.removeItem(item)
-
-            cartService.update(
-                CartSchema(
-                    id = cart.id,
-                    items = cart.items,
-                    total = cart.getTotal()
-                )
-            )
-
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(null)
-        } else {
-            throw ProductNotFoundException()
-        }
     }
 
     @DeleteMapping("/{id}")
